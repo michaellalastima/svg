cc7febadb0a09a3553eb397712325af657c5e9bf
michaellalastima <60122577+michaellalastima@users.noreply.github.com>
2020-10-12 20:00:53 +0800

    Create inddex.php

---
+++ inddex.php
@@ -0,0 +1,72 @@
+<div class="box">
+<svg>
+   <line x1=".30" y1="75" x2="80" y2="0"
+   stroke-width="4" stroke="brown"/>
+
+ <line x1="230" y1="200" x2="100" y2="0"
+   stroke-width="4" stroke="brown"/>
+  
+  <circle cx="60" cy="120" r="23" stroke="violet" stroke-width="3" fill="pink" />
+  
+  <circle cx="120" cy="120" r="23" stroke="violet" stroke-width="3" fill="pink" />
+</svg>
+</div>
+<br>
+<div class="box"> 
+<svg>
+  <rect width="200" height="90" style="fill:green; stroke-width="3" stroke="black" />                  
+     
+      <circle cx="50" cy="120" r="23" stroke="violet" stroke-width="3" fill="pink" /> 
+  
+  <circle cx="140" cy="120" r="23" stroke="violet" stroke-width="3" fill="pink" />                              </svg>     
+  <svg>
+  <rect width="200" height="90" style="fill:green; stroke-width="3" stroke="black" />
+  
+  <circle cx="50" cy="120" r="23" stroke="violet" stroke-width="3" fill="pink" /> 
+  
+  <circle cx="140" cy="120" r="23" stroke="violet" stroke-width="3" fill="pink" /> 
+  </svg>
+</div>  
+<br>
+<div class="box">
+  <svg>
+    <circle cx="30" cy="80" r="15" stroke="violet" stroke-width="3" fill="pink" />
+      
+      <circle cx="170" cy="80" r="15" stroke="violet" stroke-width="3" fill="pink" />
+      
+      <circle cx="100" cy="125" r="15" stroke="violet" stroke-width="3" fill="pink" />
+    
+<polyline points="0,40 20,20 40,40 60,20 80,40 100,20 120,40 140,20 160,40 180,20 200,40 195,40 180,25 160,45 140, 25 120,45 100, 25 80, 45 60,25 40, 45 20,25 0, 45 0,40"
+  fill="red" stroke="red" />
+    </svg>
+  
+    <svg>
+    <polyline points="0,40 20,20 40,40 60,20 80,40 100,20 120,40 140,20 160,40 180,20 200,40 195,40 180,25 160,45 140, 25 120,45 100, 25 80, 45 60,25 40, 45 20,25 0, 45 0,40"
+  fill="red" stroke="red" />
+    
+  
+  <circle cx="30" cy="80" r="15" stroke="violet" stroke-width="3" fill="pink" />
+      
+      <circle cx="170" cy="80" r="15" stroke="violet" stroke-width="3" fill="pink" />
+      
+      <circle cx="100" cy="125" r="15" stroke="violet" stroke-width="3" fill="pink" />
+    </svg>
+  </div>
+  <br>
+  <div class="box">
+  <svg>
+     <ellipse cx="100" cy="100" rx="90" ry="26" style="fill:red" />
+  <ellipse cx="110" cy="100" rx="70" ry="20" style="fill:white" />
+    
+    <polyline points="0,40 20,20 40,40 60,20 80,40 100,20 120,40 140,20 160,40 180,20 200,40 195,40 180,25 160,45 140, 25 120,45 100, 25 80, 45 60,25 40, 45 20,25 0, 45 0,40"
+  fill="green" stroke="black" />
+    </svg>
+    <svg>
+    <ellipse cx="100" cy="100" rx="90" ry="26" style="fill:red" />
+  <ellipse cx="110" cy="100" rx="70" ry="20" style="fill:white" />
+      
+      <polyline points="0,40 20,20 40,40 60,20 80,40 100,20 120,40 140,20 160,40 180,20 200,40 195,40 180,25 160,45 140, 25 120,45 100, 25 80, 45 60,25 40, 45 20,25 0, 45 0,40"
+  fill="green" stroke="black" />
+  
+  </div>
+  
